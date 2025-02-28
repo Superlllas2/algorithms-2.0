a98ddd60023d75fb0c48f9a13c0d3160cd62e2d3
Superlllas2 <64853531+Superlllas2@users.noreply.github.com>
2025-02-28 11:38:57 +0100

    2 squares made

--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -1,17 +1,32 @@
+using System.Collections.Generic;
+using NUnit.Framework;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    private RectInt room;
-    
+    private RectInt initRoom;
+    private List<RectInt> rooms;
+    private RectInt room1;
+    private RectInt room2;
+    private int gap = 10;
+
+    public int roomNum = 2;
+
     void Start()
     {
-        room = new RectInt(0, 0, 100, 50);
+        initRoom = new RectInt(0, 0, 100, 50);
+        for (var i = 0; i < roomNum; i++)
+        {
+            room1 = new RectInt(i * initRoom.width / 2, 0, initRoom.width / 2 + gap, 50);
+            room2 = new RectInt(i * initRoom.width / 2, 0, initRoom.width / 2 + gap, 50);
+        }
     }
 
     void Update()
     {
-        
+        AlgorithmsUtils.DebugRectInt(initRoom, Color.red);
+        AlgorithmsUtils.DebugRectInt(room1, Color.red);
+        AlgorithmsUtils.DebugRectInt(room2, Color.red);
     }
 }
