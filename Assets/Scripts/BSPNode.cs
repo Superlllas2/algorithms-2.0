880a7493edf03e4e1b03fbee2b7359562c4aafac
Superlllas2 <64853531+Superlllas2@users.noreply.github.com>
2025-04-04 16:40:23 +0200

    Rooms generation done

---
+++ Assets/Scripts/BSPNode.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class BSPNode
+{
+    public RectInt Bounds;
+    public BSPNode Left;
+    public BSPNode Right;
+}
--- Assets/Scripts/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGenerator.cs
@@ -5,28 +5,73 @@ using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    private RectInt initRoom;
-    private List<RectInt> rooms;
-    private RectInt room1;
-    private RectInt room2;
-    private int gap = 10;
+    public RectInt initialBounds = new RectInt(0, 0, 100, 60);
+    public int minSplitSize = 20;
+    public int maxDepth = 4;
 
-    public int roomNum = 2;
+    private BSPNode rootNode;
 
     void Start()
     {
-        initRoom = new RectInt(0, 0, 100, 50);
-        for (var i = 0; i < roomNum; i++)
-        {
-            room1 = new RectInt(i * initRoom.width / 2, 0, initRoom.width / 2 + gap, 50);
-            room2 = new RectInt(i * initRoom.width / 2, 0, initRoom.width / 2 + gap, 50);
-        }
+        rootNode = new BSPNode { Bounds = initialBounds };
+        Split(rootNode, maxDepth);
     }
 
     void Update()
     {
-        AlgorithmsUtils.DebugRectInt(initRoom, Color.red);
-        AlgorithmsUtils.DebugRectInt(room1, Color.red);
-        AlgorithmsUtils.DebugRectInt(room2, Color.red);
+        DrawDebugRects(rootNode);
+    }
+
+    void Split(BSPNode node, int depth)
+    {
+        if (depth == 0 || node.Bounds.width < minSplitSize * 2 && node.Bounds.height < minSplitSize * 2)
+            return;
+
+        bool splitHorizontally = Random.value > 0.5f;
+
+        if (node.Bounds.width > node.Bounds.height)
+            splitHorizontally = false;
+        else if (node.Bounds.height > node.Bounds.width)
+            splitHorizontally = true;
+
+        if (splitHorizontally)
+        {
+            Debug.Log("splitHorizontally");
+            int splitY = Random.Range(minSplitSize, node.Bounds.height - minSplitSize);
+            node.Left = new BSPNode
+            {
+                Bounds = new RectInt(node.Bounds.x, node.Bounds.y, node.Bounds.width, splitY)
+            };
+            node.Right = new BSPNode
+            {
+                Bounds = new RectInt(node.Bounds.x, node.Bounds.y + splitY, node.Bounds.width, node.Bounds.height - splitY)
+            };
+        }
+        else
+        {
+            Debug.Log("splitVertically");
+            int splitX = Random.Range(minSplitSize, node.Bounds.width - minSplitSize);
+            node.Left = new BSPNode
+            {
+                Bounds = new RectInt(node.Bounds.x, node.Bounds.y, splitX, node.Bounds.height)
+            };
+            node.Right = new BSPNode
+            {
+                Bounds = new RectInt(node.Bounds.x + splitX, node.Bounds.y, node.Bounds.width - splitX, node.Bounds.height)
+            };
+        }
+
+        Split(node.Left, depth - 1);
+        Split(node.Right, depth - 1);
+    }
+
+    void DrawDebugRects(BSPNode node)
+    {
+        if (node == null) return;
+
+        AlgorithmsUtils.DebugRectInt(node.Bounds, Color.red);
+
+        DrawDebugRects(node.Left);
+        DrawDebugRects(node.Right);
     }
 }
